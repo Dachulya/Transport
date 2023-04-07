280acf4463a9fa64b03aef36d94389266331f8a3
Dachulya <111542022+Dachulya@users.noreply.github.com>
2023-04-07 20:56:24 +0300

    eeee

--- src/Transport/Main.java
+++ src/Transport/Main.java
@@ -6,6 +6,10 @@ import Driver.D;
 import Transport.Enim.BodyType;
 import mechanic.Mechanic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import static Transport.Enim.CarriageCapacity.N1;
 import static Transport.Enim.CarriageCapacity.N3;
 import static Transport.Enim.PassengerCapacityBus.L;
@@ -13,81 +17,98 @@ import static Transport.Enim.PassengerCapacityBus.L;
 public class Main {
     public static void main(String[] args) {
 
-        Mechanic mechanic1=new Mechanic("Аркадий1","remontServis");
-        Mechanic mechanic2=new Mechanic("Анатолий","remontServis");
-        Mechanic mechanic3=new Mechanic("Андре","remontServis");
+        Mechanic mechanic1 = new Mechanic("Аркадий1", "remontServis");
+        Mechanic mechanic2 = new Mechanic("Анатолий", "remontServis");
+        Mechanic mechanic3 = new Mechanic("Андре", "remontServis");
 
 
-        Car ladaGranda= new Car("","","",-1,
-                0, "",true ,187,
-                4,true, new Car.Key(true,true),
-                67,6.7f, BodyType.MINIVAN, mechanic1 );
-        Car AudiA8_50LTDI= new Car("Audi","A8  50 L TDI",
-                "чёрного цвета",3f, 2020,
-                "Германия",true,187,
-                4,true,new Car.Key(true,true),
-                80,7.0f,BodyType.PICKUP,mechanic1);
-        Car BMWZ8= new Car("BMW","Z8","чёрного цвета",3,
-                2018,"Германия",true,187,
-                4,true,new Car.Key(true,true),
-                160,9.8f,BodyType.SEDAN, mechanic2);
-        Car KiaSportage= new Car("Kia","Sportage","оранжевого цвета"
-                ,2.4f, 2018,
-                "Южная Корея",true,187,
-                4,true,new Car.Key(true,true),
-                180,9.7f,BodyType.VAN,mechanic2);
-        Car HuindayAvante= new Car("красного цвета","Avante",
-                "Huinday",1.6f, 2016,
-                "Южная Корея",true,187,
-                4,true,new Car.Key(true,true),
-                170,2.4f,BodyType.HATCBACK, mechanic2);
-
+        Car ladaGranda = new Car("", "", "", -1,
+                0, "", true, 187,
+                4, true, new Car.Key(true, true),
+                67, 6.7f, BodyType.MINIVAN, mechanic1);
+        Car AudiA8_50LTDI = new Car("Audi", "A8  50 L TDI",
+                "чёрного цвета", 3f, 2020,
+                "Германия", true, 187,
+                4, true, new Car.Key(true, true),
+                80, 7.0f, BodyType.PICKUP, mechanic1);
+        Car BMWZ8 = new Car("BMW", "Z8", "чёрного цвета", 3,
+                2018, "Германия", true, 187,
+                4, true, new Car.Key(true, true),
+                160, 9.8f, BodyType.SEDAN, mechanic2);
+        Car KiaSportage = new Car("Kia", "Sportage", "оранжевого цвета"
+                , 2.4f, 2018,
+                "Южная Корея", true, 187,
+                4, true, new Car.Key(true, true),
+                180, 9.7f, BodyType.VAN, mechanic2);
+        Car HuindayAvante = new Car("красного цвета", "Avante",
+                "Huinday", 1.6f, 2016,
+                "Южная Корея", true, 187,
+                4, true, new Car.Key(true, true),
+                170, 2.4f, BodyType.HATCBACK, mechanic2,mechanic1);
 
 
         HuindayAvante.setMaxSpeed(180);
 
 
-        Bus bus1= new Bus("red","tera","bmv",70,2000,
-                "Chiny",6.8f,L,mechanic3);
+        Bus bus1 = new Bus("red", "tera", "bmv", 70, 2000,
+                "Chiny", 6.8f, L, mechanic3);
 
-        Truck truck1=new Truck("blue","truck","lada",60,
-                1990,"Rossia",10.2f,N1,mechanic3);
-        Truck truck2=new Truck("black","truck","Mercedes",80,
-                2010,"Germaniay",20.3f, N3,mechanic2);
-
+        Truck truck1 = new Truck("blue", "truck", "lada", 60,
+                1990, "Rossia", 10.2f, N1, mechanic3);
+        Truck truck2 = new Truck("black", "truck", "Mercedes", 80,
+                2010, "Germaniay", 20.3f, N3, mechanic2);
 
 
         //bus1.Stop();
-        A driver1=new A(true,34,"Пётр Сергеевич","", HuindayAvante);
-        B driver2=new B<>(true,34,"Анатолий Павлович","",truck1);
-        B driver3=new B<>(true,25,"Павел Вениаминович","",truck2);
-        D driver4=new D<>(true,7,"Ангелина Анатольевна","",bus1);
+        A driver1 = new A(true, 34, "Пётр Сергеевич", "", HuindayAvante);
+        B driver2 = new B<>(true, 34, "Анатолий Павлович", "", truck1);
+        B driver3 = new B<>(true, 25, "Павел Вениаминович", "", truck2);
+        D driver4 = new D<>(true, 7, "Ангелина Анатольевна", "", bus1);
 
-        carryOutTransport(bus1,HuindayAvante,truck1,truck2);
-
+        printMechanicsForCar(
+                createHashMapCarWithMechanic(HuindayAvante,BMWZ8,AudiA8_50LTDI)
+        );
     }
 
-    public static void carryOutTransport(Transport...transports){
-        ServiceStation serviceStation=new ServiceStation();
-        for (Transport transport:transports){
+    public static HashMap<Car, ArrayList<Mechanic>> createHashMapCarWithMechanic(Car... cars) {
+        HashMap<Car, ArrayList<Mechanic>> hashMap = new HashMap<>();
+        for (Car car : cars) {
+            hashMap.put(car, car.getMechanics());
+        }
+        return hashMap;
+    }
+    public static void printMechanicsForCar(HashMap<Car, ArrayList<Mechanic>> hashMap){
+        for (Map.Entry<Car,ArrayList<Mechanic>> entry:hashMap.entrySet()){
+            System.out.println(entry.getKey()+" :");
+            for (Mechanic mechanic:entry.getValue()) {
+                System.out.println(mechanic);
+            }
+        }
+    }
+
+    public static void carryOutTransport(Transport... transports) {
+        ServiceStation serviceStation = new ServiceStation();
+        for (Transport transport : transports) {
             serviceStation.addTransport(transport);
         }
         serviceStation.carryOutATechnicalInspection();
     }
-    public static void checkTransport(Transport...transports){
-        int count=0;
-        for (Transport transport:transports){
-            if (!transport.passDiagnostics()){
-                try{
-                    throw new RuntimeException(transport.getBrend()+" "+
-                            transport.getModel()+" не прошёл диагностику");
-                }catch (RuntimeException e){
-                e.printStackTrace();
-            }}else {
+
+    public static void checkTransport(Transport... transports) {
+        int count = 0;
+        for (Transport transport : transports) {
+            if (!transport.passDiagnostics()) {
+                try {
+                    throw new RuntimeException(transport.getBrend() + " " +
+                            transport.getModel() + " не прошёл диагностику");
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+            } else {
                 count++;
             }
         }
-        System.out.println("диагностику прошли"+ count
-                +" из "+transports.length+" автомобилей ");
+        System.out.println("диагностику прошли" + count
+                + " из " + transports.length + " автомобилей ");
     }
 }
