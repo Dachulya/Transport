5c5efde6848a3a6c6eca9e3e0e624206b3da2699
Dachulya <111542022+Dachulya@users.noreply.github.com>
2023-04-08 18:53:56 +0300

    eeee

--- src/Transport/ServiceStation.java
+++ src/Transport/ServiceStation.java
@@ -2,6 +2,7 @@ package Transport;
 
 
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Queue;
 
 public class ServiceStation {
@@ -31,4 +32,16 @@ public class ServiceStation {
 Е роll() - возвращает элемент из головы очереди и удаляет его. Возвращает null, если очередь пуста.
 boolean offer(Е оbj) - пытается добавить оbj в очередь. Возвращает true, если оbj добавлен, и false в противном случае.*/
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServiceStation that = (ServiceStation) o;
+        return Objects.equals(transport, that.transport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transport);
+    }
 }
